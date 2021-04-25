e86199d61c0163c40169d6d3141609a2ed379321
Luca Saiu <positron@gnu.org>
2021-04-26 00:36:39 +0200

    add minimal-threading support for ARM
    The lack of a hardware division instruction will provide for some testing
    opportunities in this case.
    
    * Makefile.am (EXTRA_DIST): Distribute new files.
    
    * configure.ac (JITTER_ASSEMBLY_SUBDIRECTORY, jitter_supports_minimal_threading)
    [arm]: Define.
    
    * machine/arm/jitter/machine/jitter-machine-assembly.S: New file.
    * machine/arm/jitter/machine/jitter-machine-c.c: New file.
    * machine/arm/jitter/machine/jitter-machine.h: New file.

---
+++ machine/arm/jitter/machine/jitter-machine-c.c
@@ -0,0 +1,23 @@
+/* VM library: native code patching for a minimal-threading-only architecture.
+
+   Copyright (C) 2021 Luca Saiu
+   Written by Luca Saiu
+
+   This file is part of Jitter.
+
+   Jitter is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   Jitter is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with Jitter.  If not, see <http://www.gnu.org/licenses/>. */
+
+
+/* Nothing useful here: this architecture currently has only minimal-threading
+   support. */
