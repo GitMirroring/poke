51810d3bfa3096e9e65ea4219245e9b3777d541c
Jose E. Marchesi <jose.marchesi@oracle.com>
2024-09-21 00:30:41 +0200

    pkl-typify: fix memory leak in pkl_typify1_ps_struct_type_field
    2024-09-21  Jose E. Marchesi  <jemarch@gnu.org>
    
    	* libpoke/pkl-typify.c (pkl_typify1_ps_struct_type_field): Fix
    	memory leak.

--- libpoke/pkl-typify.c
+++ libpoke/pkl-typify.c
@@ -3000,6 +3000,7 @@ PKL_PHASE_BEGIN_HANDLER (pkl_typify1_ps_struct_type_field)
      an offset<uint<64>,*>.  */
   if (elem_label)
     {
+      int promoteable_p = 0;
       pkl_ast_node label_type = PKL_AST_TYPE (elem_label);
       pkl_ast_node offset_type
         = pkl_ast_make_offset_type (PKL_PASS_AST,
@@ -3007,9 +3008,10 @@ PKL_PHASE_BEGIN_HANDLER (pkl_typify1_ps_struct_type_field)
                                     pkl_ast_make_integer (PKL_PASS_AST, 1),
                                     NULL /* ref_type */);
 
-
-      if (!pkl_ast_type_promoteable_p (label_type, offset_type,
-                                       1 /* promote_array_of_any */))
+      promoteable_p = pkl_ast_type_promoteable_p (label_type, offset_type,
+                                                  1 /* promote_array_of_any */);
+      offset_type = ASTREF (offset_type); pkl_ast_node_free (offset_type);
+      if (!promoteable_p)
         {
           char *type_str = pkl_type_str (label_type, 1);
 
@@ -3021,8 +3023,6 @@ PKL_PHASE_BEGIN_HANDLER (pkl_typify1_ps_struct_type_field)
           PKL_TYPIFY_PAYLOAD->errors++;
           PKL_PASS_ERROR;
         }
-
-      offset_type = ASTREF (offset_type); pkl_ast_node_free (offset_type);
     }
 }
 PKL_PHASE_END_HANDLER
