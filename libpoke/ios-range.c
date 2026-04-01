933c6629c05dbed4ce05cbce22155ad676cc5b57
David Faust <david.faust@oracle.com>
2026-04-01 15:13:33 -0700

    add a copyright header to ios-range.c

--- libpoke/ios-range.c
+++ libpoke/ios-range.c
@@ -1,3 +1,22 @@
+/* ios-range.c - IO mapped value range tracking for poke.  */
+
+/* Copyright (C) 2026 Free Software Foundation, Inc.
+   Written by David Faust <david.faust@oracle.com>, 2026.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include "pk-utils.h"
 #include "ios.h"
 #include "ios-dev.h"
